34a6f6ec4413b504b72d279c34594997dc2f458a
Junhyeong Lim <entrypoint_@naver.com>
2017-02-21 15:00:00 +0900

    Change event

--- src/main/java/kr/rvs/pscontroller/listener/EventListener.java
+++ src/main/java/kr/rvs/pscontroller/listener/EventListener.java
@@ -3,19 +3,18 @@ package kr.rvs.pscontroller.listener;
 import kr.rvs.pscontroller.handler.ConfigHandler;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import protocolsupport.api.ProtocolSupportAPI;
 import protocolsupport.api.ProtocolVersion;
+import protocolsupport.api.events.PlayerSyncLoginEvent;
 
 /**
  * Created by deide on 2016-11-22.
  */
 public class EventListener implements Listener {
     @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        ProtocolVersion version = ProtocolSupportAPI.getProtocolVersion(e.getPlayer());
+    public void onLogin(PlayerSyncLoginEvent e) {
+        ProtocolVersion version = e.getConnection().getVersion();
         if (!ConfigHandler.isAllow(version)) {
-            e.getPlayer().kickPlayer(ConfigHandler.getKickMsgTitle() + "\n" +
+            e.denyLogin(ConfigHandler.getKickMsgTitle() + "\n" +
                     version.getName() + " §fversion is not allowed");
         }
     }
